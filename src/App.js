b70ded1aa48e6ea613906226e0904250ca5544a7
barisenak <mariya1999-7@mail.ru>
2020-11-04 23:53:51 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,23 @@
 import "./App.css";
 import React from "react";
 
+class App extends React.Component {
+  state = {
+    isSignedIn: false,
+  };
+
+  change = () => {
+    this.setState({ isSignedIn: true });
+    setTimeout(this.change, 3000);
+  };
+
+  render() {
+    return this.state.isSignedIn ? (
+      <p>Hello!</p>
+    ) : (
+      <button onClick={this.change}>Sign In</button>
+    );
+  }
+}
+
 export default App;
